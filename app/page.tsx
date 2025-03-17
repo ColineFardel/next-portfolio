676b63b3e4b523a5a1769f43f228f84f0617d218
ColineFardel <coline.fardel@gmail.com>
2025-03-17 09:02:26 +0100

    Initi commit

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <header>ajlkja</header>
+      <section id="home" className="bg-pink-500 h-screen flex">
+        <div className="text-white p-10 border-1 border-black h-fit flex flex-col">
+          <span>Hello !</span>
+          <span> klajéefa jejféao iewjfoédfjvaoiej ocivj we oaiiojaeo aéosijf </span>
+        </div>
+        <div>blablalblablalb klabkbjklaejrklva jalkergklaegv ekrj ke elkr lea vjaekjrkf dlkvoeriogja lkcvjoisjerk vkjeoirjga ojevnoeirn knvijeorg</div>
+        <div>Image</div>
+      </section>
+      <section id="stack" className="h-screen">
+        blabalblabla
+      </section>
+      <section id="portfolio" className="h-screen bg-pink-500">
+        blabalblabla
+      </section>
+      <section id="contact" className="h-screen">
+        blabalblabla
+      </section>
+    </div>
+  );
+}
