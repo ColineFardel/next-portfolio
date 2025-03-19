572e364e3f2ace3cb5a35d3a6971473a86ec92e7
ColineFardel <coline.fardel@gmail.com>
2025-03-19 15:47:30 +0100

    Redo skills design

--- app/components/icon-card.tsx
+++ app/components/icon-card.tsx
@@ -1,8 +1,21 @@
-export default function IconCard({ icon }: { icon: string }) {
+export default function IconCard({ icon, name, knowledge }: { icon: string, name: string, knowledge: number }) {
+
+    const starArray = Array.from({ length: knowledge }, (element, index) => index);
+    const emptyArray = Array.from({ length: 5 - knowledge }, (element, index) => index);
 
     return (
-        <div className="bg-(--secondary) p-4 rounded-lg w-[60px] h-[60px]">
-            <img src={icon} alt="" />
+        <div className="icon-card relative">
+            <div className="absolute w-[90%] h-[90%] left-[50%] top-[50%] transform-[translate(-50%,-50%)]">
+                <img className="icon-img w-full h-full object-contain" src={'./' + icon + '.svg'} alt="" />
+            </div>
+
+            <div className="hidden-text w-full h-full flex flex-col gap-2 items-center justify-center">
+                <div>{name}</div>
+                <div className="">
+                    {starArray.map((el) => <img className="inline" src="./star-full-light.svg" />)}
+                    {emptyArray.map((el) => <img className="inline" src="./star-empty-light.svg" />)}
+                </div>
+            </div>
         </div>
     )
 }
--- app/components/skills.tsx
+++ app/components/skills.tsx
@@ -3,41 +3,110 @@ import IconCard from "./icon-card";
 
 export default function Skills() {
 
-    const icons = {
-        database: ['./firebase.svg', './postgre.svg', './database.svg'],
-        frontend: ['./react.svg', './angular.svg', './css.svg', './html.svg', './ts.svg'],
-        backend: ['./java.svg', './spring.svg', './js.svg'],
-        other: ['./jira.svg', './docker.svg']
-    }
+    const data = [
+        {
+            name: 'Database',
+            nbrCol: 'col-span-4',
+            stack: [
+                {
+                    name: 'Firebase',
+                    icon: 'firebase',
+                    knowledge: 3
+                },
+                {
+                    name: 'PostgreSQL',
+                    icon: 'postgre',
+                    knowledge: 2
+                },
+                {
+                    name: 'MySQL',
+                    icon: 'database',
+                    knowledge: 2
+                },
+            ]
+        },
+        {
+            name: 'Backend',
+            nbrCol: 'col-span-4',
+            stack: [
+                {
+                    name: 'Java',
+                    icon: 'java',
+                    knowledge: 4
+                },
+                {
+                    name: 'Spring Boot',
+                    icon: 'spring',
+                    knowledge: 3
+                },
+                {
+                    name: 'JavaScript',
+                    icon: 'js',
+                    knowledge: 4
+                },
+            ]
+        },
+        {
+            name: 'Frontend',
+            nbrCol: 'col-span-5',
+            stack: [
+                {
+                    name: 'React',
+                    icon: 'react',
+                    knowledge: 3
+                },
+                {
+                    name: 'Angular',
+                    icon: 'angular',
+                    knowledge: 4
+                },
+                {
+                    name: 'CSS',
+                    icon: 'css',
+                    knowledge: 2
+                },
+                {
+                    name: 'HTML',
+                    icon: 'html',
+                    knowledge: 2
+                },
+                {
+                    name: 'TypeScript',
+                    icon: 'ts',
+                    knowledge: 3
+                },
+            ]
+        },
+        {
+            name: 'Other',
+            nbrCol: 'col-span-3',
+            stack: [
+                {
+                    name: 'Jira',
+                    icon: 'jira',
+                    knowledge: 2
+                },
+                {
+                    name: 'Docker',
+                    icon: 'docker',
+                    knowledge: 2
+                },
+            ]
+        },
+    ]
 
     return (
-        <section id="skills" className="h-screen bg-(--secondary) flex flex-col justify-start">
+        <section id="skills" className="h-screen bg-(--secondary) flex flex-col">
             <Title title="Skills" />
-            <div className="grid grid-cols-6 grid-rows-2 gap-2 m-7">
-                <div className="col-span-3 skill-container">
-                    Database
-                    <div className="flex justify-around items-center">
-                        {icons.database.map((icon) => <IconCard key={icon} icon={icon} />)}
+            <div className="grid grid-cols-8 grid-rows-2 gap-2 mx-7 my-auto">
+                {data.map((skillData) =>
+                    <div key={skillData.name} className={'skill-container ' + skillData.nbrCol}>
+                        <div className="text-lg font-bold">{skillData.name}</div>
+                        <div className="icons-container">
+                            {skillData.stack.map((item) => <IconCard key={item.icon} icon={item.icon} name={item.name} knowledge={item.knowledge} />)}
+                        </div>
                     </div>
-                </div>
-                <div className="col-span-3 skill-container">
-                    Backend
-                    <div className="flex justify-around items-center">
-                        {icons.backend.map((icon) => <IconCard key={icon} icon={icon} />)}
-                    </div>
-                </div>
-                <div className="col-span-4 skill-container">
-                    Frontend
-                    <div className="flex justify-around items-center">
-                        {icons.frontend.map((icon) => <IconCard key={icon} icon={icon} />)}
-                    </div>
-                </div>
-                <div className="col-span-2 skill-container">
-                    Other
-                    <div className="flex justify-around items-center">
-                        {icons.other.map((icon) => <IconCard key={icon} icon={icon} />)}
-                    </div>
-                </div>
+                )}
             </div>
 
         </section>
